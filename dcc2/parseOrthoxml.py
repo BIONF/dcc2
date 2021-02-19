aa3c50070a29b67e6f503139f42fab72da2a8a7b
trvinh <trvinh@gmail.com>
2021-02-19 10:28:15 +0100

    version bump

--- dcc2/parseOrthoxml.py
+++ dcc2/parseOrthoxml.py
@@ -49,7 +49,7 @@ def concatFasta(fileIn, fileOut):
      subprocess.call([cmd], shell = True)
 
 def main():
-    version = "0.0.8"
+    version = "0.0.9"
     parser = argparse.ArgumentParser(description="You are running dcc2 version " + str(version))
     required = parser.add_argument_group('required arguments')
     optional = parser.add_argument_group('additional arguments')
